5aa01dfde35736968358673ac66bcdb01a3ba266
laxmi <waghmorelaxmi@gmail.com>
2022-08-28 01:38:30 +0530

    first part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import {Section } from './components/Section'
+import {First } from './components/First'
 
 function App() {
   return (
     <div className="App">
       <Section/>
+      <First/>
      
     </div>
   );
